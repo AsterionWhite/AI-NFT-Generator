8bdccad88c1b9a790f82c79ad5e1a0b46cadec98
AsterionWhite <crypto.asterion@gmail.com>
2023-02-26 14:30:52 +0100

    ipfs initialization and prep for file upload

--- src/GeneratedGrid.js
+++ src/GeneratedGrid.js
@@ -1,8 +1,38 @@
 import "./GeneratedGrid.css";
+import * as IPFS from 'ipfs-core';
+import { useEffect } from "react";
 
 const GeneratedGrid = ({ generatedImages }) => {
   const underlyingGridCards = Array(4).fill({ url: null });
 
+  let ipfs = null;
+
+  useEffect(() => {
+    greateIpfsNode();
+  }, [])
+
+  const greateIpfsNode = async() => {
+    try {
+      if(!ipfs) {
+        ipfs = await IPFS.create({repo: 'ok'+ Math.random()})
+      }
+      
+    } catch (error) {
+      console.log(error, 'ipfs create error')
+    }
+  }
+
+
+  const selectImage = async(imageUrl) => {
+    console.log(imageUrl, 'ur; ???');
+    let file;
+    if(ipfs) {
+      file = await ipfs.add(IPFS.urlSource(imageUrl));
+    }
+
+    console.log(file, 'file ...')
+  }
+  console.log(ipfs, 'ipfs maina ????')
   return (
     <div className="grid-container">
       <div className="underlying-grid-wrapper">
@@ -21,7 +51,7 @@ const GeneratedGrid = ({ generatedImages }) => {
           ) : (
             <div className="generated-image" src={image.url} />
           );
-          return <div key={`generated-image-${idx}`}>{nftCardElement}</div>;
+          return <div onClick={() => selectImage(image.url)} key={`generated-image-${idx}`}>{nftCardElement}</div>;
         })}
       </div>
     </div>
